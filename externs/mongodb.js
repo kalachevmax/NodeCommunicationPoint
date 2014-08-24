65441429f05c799bd700182483d3012c61af930f
Maxim Kalachev <maxkalachev@yandex.ru>
2014-08-25 01:05:38 +0400

    ncp.db.MongoClient

---
+++ externs/mongodb.js
@@ -0,0 +1,27 @@
+
+
+/**
+ * @namespace
+ */
+var mongodb = {};
+
+
+/**
+ * @typedef {*}
+ */
+mongodb.DataBase;
+
+
+/**
+ * @constructor
+ */
+mongodb.MongoClient = function() {};
+
+
+/**
+ * @param {string} url
+ * @param {!Object|!Function} options
+ * @param {!Function=} opt_callback
+ */
+mongodb.MongoClient.connect = function(url, options, opt_callback) {};
+
